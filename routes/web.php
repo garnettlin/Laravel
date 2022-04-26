c06a7495725c2191ec99b3857919b4a4b39fbe80
CHEN TSUNGLIN <garnettlin21@gmail.com>
2022-04-27 04:03:33 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use App\Http\Controllers\FullCalendarController;
 
-//Route::get('/', function () {
-    //return view('welcome');
-//});
-Route::get('fullcalendar', [FullCalendarController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
+//Route::get('fullcalendar', [FullCalendarController::class, 'index']);
 Route::post('fullcalendar/create', [FullCalendarController::class, 'create']);
 Route::post('fullcalendar/update', [FullCalendarController::class, 'update']);
 Route::post('fullcalendar/delete', [FullCalendarController::class, 'destroy']);
