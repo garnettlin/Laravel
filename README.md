c8f83cae56b1414141d3e295cb1d38bf7db8c4aa
gituser <garnettlin21@gmail.com>
2022-04-27 02:01:58 +0800

    demo

--- README.md
+++
@@ -1,2 +0,0 @@
-# Laravel
-laravel-tests
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\FullCalendarController;
+
+
+Route::get('fullcalendar', [FullCalendarController::class, 'index']);
+Route::post('fullcalendar/create', [FullCalendarController::class, 'create']);
+Route::post('fullcalendar/update', [FullCalendarController::class, 'update']);
+Route::post('fullcalendar/delete', [FullCalendarController::class, 'destroy']);
+
